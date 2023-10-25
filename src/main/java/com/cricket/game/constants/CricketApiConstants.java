b3079f1a1df0480b64e713e1084d4b6270dc4e24
vimaln <vimal2009n@gmail.com>
2023-10-25 13:32:23 +0530

    phase 2 changes

--- src/main/java/com/cricket/game/constants/CricketApiConstants.java
+++ src/main/java/com/cricket/game/constants/CricketApiConstants.java
@@ -2,5 +2,7 @@ package com.cricket.game.constants;
 
 public class CricketApiConstants {
 
-    public static final String GET_MATCH_RESULT="/getTeamResult";
+    public static final String CRICKET_API="/api/match/";
+    public static final String GET_MATCH_RESULT="/getMatchResult";
+
 }
